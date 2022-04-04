af8e74a5e89954de7e69f9ff78356742a60939ce
manuel pascua <manuelpascua@MacBook-Pro-de-manuel-2.local>
2022-04-04 10:27:54 -0300

    ka

--- myapp/app.js
+++ myapp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const mainRouter = require('./routes/mainRouter');
+const mainRouter = require('./routes/main');
 
 
 var app = express();
