dcf36a7f8fa74988151d0c5716e2819ba8638b5d
marcoswilson1 <mwilson@udesa.edu.ar>
2022-04-11 10:36:44 -0300

    agregue informacion al db

--- myapp/db/db.js
+++ myapp/db/db.js
@@ -1,80 +1,79 @@
 const data = {
     usuario: {
-        email: '',
-        usuario: '',
-        fecha_de_nacimiento: '',
-        nro_de_documento: '', 
+        email: 'mwilson@udesa.edu.ar',
+        usuario: 'marcoswilson1',
+        fecha_de_nacimiento: '10/05/2002',
+        nro_de_documento: '448380209', 
         foto_de_perfil: '',
-        contrasenia: '',
-        }
-}
+        contrasenia: '123456789',
+        },
 
 
-const productos = [{
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+productos: [{
+    imagen: '/images/products/Audi.jpeg',
+    Nombre_del_producto: 'Audi',
+    descripción: 'A4',
+    Fecha_de_carga: '20/10/2020',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Audi2.jpeg',
+    Nombre_del_producto: 'Audi',
+    descripción: 'A6',
+    Fecha_de_carga: '15/09/2020',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Alfa-romeo.jpeg',
+    Nombre_del_producto: 'Alfa romeo',
+    descripción: 'Giulia',
+    Fecha_de_carga: '19/04/2021',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/BMW.jpeg',
+    Nombre_del_producto: 'BMW',
+    descripción: 'Serie 2',
+    Fecha_de_carga: '30/01/2022',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Chery.jpeg',
+    Nombre_del_producto: 'Chery',
+    descripción: 'Tiggo 2',
+    Fecha_de_carga: '06/03/2019',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/DS9.jpeg',
+    Nombre_del_producto: 'DS',
+    descripción: '9',
+    Fecha_de_carga: '25/05/2020',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Ford.jpeg',
+    Nombre_del_producto: 'Ford',
+    descripción: 'Bronco sport',
+    Fecha_de_carga: '19/10/2021',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Maserati.jpeg',
+    Nombre_del_producto: 'Maserati',
+    descripción: 'Quattroporte',
+    Fecha_de_carga: '20/02/2022',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
+    imagen: '/images/products/Nissan.jpeg',
+    Nombre_del_producto: 'Nissan',
+    descripción: 'Sentra',
+    Fecha_de_carga: '31/03/2020',
 },
 {
-    imagen: '',
-    Nombre_del_producto: '',
-    descripción: '',
-    Fecha_de_carga: '',
-},]
+    imagen: '/images/products/Toyota.jpeg',
+    Nombre_del_producto: 'Toyota',
+    descripción: 'Corolla',
+    Fecha_de_carga: '08/05/2018',
+}],
 
 
-const comentarios = [{
-    nombre_de_usuario: '',
-    texto_del_comentario: '',
+comentarios:  [{
+    nombre_de_usuario: 'vugugiho',
+    texto_del_comentario: 'uvrjxdeo',
     imagen_de_perfil: '',
 }]
-
+}
