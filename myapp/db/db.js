4da1f149698fa811d53f1ee088b06e5871bdbf06
marcoswilson1 <mwilson@udesa.edu.ar>
2022-04-11 11:32:33 -0300

    Carpeta de data base terminada

--- myapp/db/db.js
+++ myapp/db/db.js
@@ -1,7 +1,7 @@
 const data = {
     usuario: {
-        email: 'nrjfnjr1udesa.edu.ar',
-        usuario: 'nrjfnjr1',
+        email: 'marcos123@udesa.edu.ar',
+        usuario: 'marcos',
         fecha_de_nacimiento: '10/05/2002',
         nro_de_documento: '448380209', 
         foto_de_perfil: '/images/users/fotodepp.jpeg',
@@ -9,71 +9,118 @@ const data = {
         },
 
 
-productos: [{
-    imagen: '/images/products/Audi.jpeg',
-    Nombre_del_producto: 'Audi',
-    descripción: 'A4',
-    Fecha_de_carga: '20/10/2020',
+    productos: [{
+        imagen: '/images/products/Audi.jpeg',
+        Nombre_del_producto: 'Audi',
+        descripción: 'A4',
+        Fecha_de_carga: '20/10/2020',
 },
 {
-    imagen: '/images/products/Audi2.jpeg',
-    Nombre_del_producto: 'Audi',
-    descripción: 'A6',
-    Fecha_de_carga: '15/09/2020',
+        imagen: '/images/products/Audi2.jpeg',
+        Nombre_del_producto: 'Audi',
+        descripción: 'A6',
+        Fecha_de_carga: '15/09/2020',
 },
 {
-    imagen: '/images/products/Alfa-romeo.jpeg',
-    Nombre_del_producto: 'Alfa romeo',
-    descripción: 'Giulia',
-    Fecha_de_carga: '19/04/2021',
+        imagen: '/images/products/Alfa-romeo.jpeg',
+        Nombre_del_producto: 'Alfa romeo',
+        descripción: 'Giulia',
+        Fecha_de_carga: '19/04/2021',
 },
 {
-    imagen: '/images/products/BMW.jpeg',
-    Nombre_del_producto: 'BMW',
-    descripción: 'Serie 2',
-    Fecha_de_carga: '30/01/2022',
+        imagen: '/images/products/BMW.jpeg',
+        Nombre_del_producto: 'BMW',
+        descripción: 'Serie 2',
+        Fecha_de_carga: '30/01/2022',
 },
 {
-    imagen: '/images/products/Chery.jpeg',
-    Nombre_del_producto: 'Chery',
-    descripción: 'Tiggo 2',
-    Fecha_de_carga: '06/03/2019',
+        imagen: '/images/products/Chery.jpeg',
+        Nombre_del_producto: 'Chery',
+        descripción: 'Tiggo 2',
+        Fecha_de_carga: '06/03/2019',
 },
 {
-    imagen: '/images/products/DS9.jpeg',
-    Nombre_del_producto: 'DS',
-    descripción: '9',
-    Fecha_de_carga: '25/05/2020',
+        imagen: '/images/products/DS9.jpeg',
+        Nombre_del_producto: 'DS',
+        descripción: '9',
+        Fecha_de_carga: '25/05/2020',
 },
 {
-    imagen: '/images/products/Ford.jpeg',
-    Nombre_del_producto: 'Ford',
-    descripción: 'Bronco sport',
-    Fecha_de_carga: '19/10/2021',
+        imagen: '/images/products/Ford.jpeg',
+        Nombre_del_producto: 'Ford',
+        descripción: 'Bronco sport',
+        Fecha_de_carga: '19/10/2021',
 },
 {
-    imagen: '/images/products/Maserati.jpeg',
-    Nombre_del_producto: 'Maserati',
-    descripción: 'Quattroporte',
-    Fecha_de_carga: '20/02/2022',
+        imagen: '/images/products/Maserati.jpeg',
+        Nombre_del_producto: 'Maserati',
+        descripción: 'Quattroporte',
+        Fecha_de_carga: '20/02/2022',
 },
 {
-    imagen: '/images/products/Nissan.jpeg',
-    Nombre_del_producto: 'Nissan',
-    descripción: 'Sentra',
-    Fecha_de_carga: '31/03/2020',
+        imagen: '/images/products/Nissan.jpeg',
+        Nombre_del_producto: 'Nissan',
+        descripción: 'Sentra',
+        Fecha_de_carga: '31/03/2020',
 },
 {
-    imagen: '/images/products/Toyota.jpeg',
-    Nombre_del_producto: 'Toyota',
-    descripción: 'Corolla',
-    Fecha_de_carga: '08/05/2018',
+        imagen: '/images/products/Toyota.jpeg',
+        Nombre_del_producto: 'Toyota',
+        descripción: 'Corolla',
+        Fecha_de_carga: '08/05/2018',
 }],
 
 
-comentarios:  [{
-    nombre_de_usuario: 'vugugiho',
-    texto_del_comentario: 'uvrjxdeo',
-    imagen_de_perfil: '/images/users/fotodepp.jpeg',
-}]
-}
+    comentarios:  [{
+        nombre_de_usuario: 'Mateo',
+        texto_del_comentario: 'Muy buen disenio',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Julio',
+        texto_del_comentario: 'Buena pintura tiene el auto',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Santiago',
+        texto_del_comentario: 'Se basa en la aceleracione el auto',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Lucas',
+        texto_del_comentario: 'Le falta una pieza en el volante',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Manuel',
+        texto_del_comentario: 'No es lo que esperaba...',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Segundo',
+        texto_del_comentario: 'Me sorprendio para bien los detalles de los faros',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Joaquin',
+        texto_del_comentario: 'Tiene muy buena velocidad constante',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Jeronimo',
+        texto_del_comentario: 'Me dejo que desear el auto',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Jorge',
+        texto_del_comentario: 'Esta muy exijido el auto',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+{
+        nombre_de_usuario: 'Dimas',
+        texto_del_comentario: 'Hace ruidos raros, parece que tiene una falla...',
+        imagen_de_perfil: '/images/users/fotodepp.jpeg',
+},
+]}
+
+module.exports = data
